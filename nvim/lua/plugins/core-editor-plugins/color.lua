b5919a616257ea917da5df6706703e530cb013e0
rasmus842 <rasmuszalite@gmail.com>
2026-01-05 06:30:28 +0200

    Gruvbox to neovim

--- nvim/lua/plugins/core-editor-plugins/color.lua
+++ nvim/lua/plugins/core-editor-plugins/color.lua
@@ -1,3 +1,53 @@
+local gruvbox = {
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		local p = require("gruvbox").palette
+		local is_strings_italic = false
+		require("gruvbox").setup({
+			terminal_colors = true, -- add neovim terminal colors
+			contrast = "", -- can be "hard", "soft" or empty string
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = is_strings_italic,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			dim_inactive = false,
+			transparent_mode = false,
+			palette_overrides = {},
+			overrides = {
+				String = { fg = p.bright_yellow, italic = is_strings_italic },
+				SignColumn = { bg = "NONE" },
+				NormalFloat = { bg = p.dark1 },
+			},
+		})
+		vim.cmd.colorscheme("gruvbox")
+	end,
+}
+
+local gruvbox_material = {
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_foreground = "original"
+		vim.g.gruvbox_material_background = "medium"
+		vim.g.gruvbox_material_enable_italic = true
+		-- vim.g.gruvbox_material_better_performance = 1
+		vim.cmd.colorscheme("gruvbox-material")
+	end,
+}
+
 local tokyonight = {
 	"folke/tokyonight.nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -68,4 +118,4 @@ local gruber_darker2 = {
 -- change the command in the config to whatever the name of that colorscheme is.
 --
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-return tokyonight
+return gruvbox
