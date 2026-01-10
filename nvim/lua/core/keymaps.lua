4227246105ab8eb9bc98be01256b3c7402ad8abe
rasmus842 <rasmuszalite@gmail.com>
2026-01-10 21:43:01 +0200

    Updates: Neovim v0.11.5 and plugins

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -78,7 +78,7 @@ vim.keymap.set("v", "p", '"_dP')
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 	group = highlight_group,
 	pattern = "*",
