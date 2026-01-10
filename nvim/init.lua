777c1f2901fb920753aa1d65d7506aebc9db75ff
rasmus842 <rasmuszalite@gmail.com>
2026-01-10 20:50:39 +0200

    Revert "Add typescript support"
    This reverts commit fdee48211904fd35ed9b566c75ed9c2196ebdec0.

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,6 @@ require("lazy").setup({
 	require("plugins.code-support.autoformatting"), -- Autoformat
 	require("plugins.code-support.autocompletion"), -- Autocomplete
 	require("plugins.code-support.html-autotags"), -- Autoclose html tags
-	require("plugins.code-support.typescrit-tools"),
 
 	-- Git integration:
 	require("plugins.git.gitsigns"), -- git signs
@@ -49,7 +48,7 @@ require("lazy").setup({
 	},
 	{
 		-- Automatic vim sessions on layout changes and exits
-		"tpope/vim-obsession",
+		"tpope/vim-obsession"
 	},
 
 	-- Highlight todo, notes, etc in comments
