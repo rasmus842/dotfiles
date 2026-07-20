100b9b1f70dc42b7d35bf862058bd6b21b84c17e
Rasmus Zalite <rasmus.zalite@glia.com>
2026-07-20 20:51:10 +0300

    Implementing relative file path copying

--- nvim/lua/custom/copy-file-path.lua
+++ nvim/lua/custom/copy-file-path.lua
@@ -1,7 +1,19 @@
 -- Copy the current buffer's absolute file path to the system clipboard.
--- Usage: :CopyFilePath
-vim.api.nvim_create_user_command("CopyFilePath", function()
+-- Usage: :FilePath
+vim.api.nvim_create_user_command("FilePath", function()
 	local path = vim.fn.expand("%:p")
+	if path == "" then
+		print("No file path for the current buffer")
+		return
+	end
+	vim.fn.setreg("+", path)
+	print("Copied: " .. path)
+end, {})
+
+-- Copy the current buffer's path relative to the working directory.
+-- Usage: :RelativePath
+vim.api.nvim_create_user_command("RelativePath", function()
+	local path = vim.fn.expand("%:.")
 	if path == "" then
 		print("No file path for the current buffer")
 		return
